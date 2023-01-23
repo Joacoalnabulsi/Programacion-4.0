661c7ef4408c0831e75f412ccce3c7e78f87a775
Joaquin Al Nabulsi <96397160+Joacoalnabulsi@users.noreply.github.com>
2023-01-23 13:35:10 -0300

    Fixed

--- data.js
+++ data.js
@@ -1,71 +1,37 @@
- const info = document.getElementById('info')
- const caret = document.getElementById('icon1')
- const caret2 = document.getElementById('icon2')
- const url = document.getElementById('click')
- const displayInfo2 = ()=>{
-caret2.addEventListener('click',(e)=>{
-  e.preventDefault(e)
-  
-   if(info.style.display ==='none'){
-    info.style.display ='block'
-    caret2.style.display ='none'
-    caret.style.display ='block'
-   }else{
-    info.style.display ='none'
-    caret.style.display ='none'
-    caret2.style.display ='block'
-   };
-})};
+//https://randomuser.me/api/
 
- const displayInfo = ()=>{
- caret.addEventListener('click',(e)=>{
-  
-  e.preventDefault(e)
-   if(info.style.display ==='none'){
-    info.style.display ='block'
-    caret2.style.display ='none'
-    caret.style.display ='block'
-   }else{
-    info.style.display ='none'
-    caret.style.display ='none'
-    caret2.style.display ='block'
-   };
-})};
+function fetchData() {
+  return fetch('https://randomuser.me/api/')
+  .then(res => res.json());
+};
 
 
 
+function renderContent (content) {
+ let phone;
+  let image;
+  let name;
+  let city;
+  let email;
 
-  function fetchData() {
-  fetch('https://randomuser.me/api/')
-  .then(res => res.json())
-  .then(data  => {
-    let author = data.results;
-    let output = "";
-    let name = "";
-    author.forEach(function Render(lists){
-      output+= `
-      <ul id="info-list" class="info-list">
-       <li>Age: ${lists.dob.age}</li>
-       <li>Email: ${lists.email}</li>
-       <li>City: ${lists.location.city}</li>
-       <li>State: ${lists.location.state} </li>
-       <li>Cellphone: ${lists.cell}</li>
-      </ul>
-      `
-      name = `${lists.name.first} ${lists.name.last}`
-      image =`<img class="photo-large" src="${lists.picture.large}">`
-    });
-    document.getElementById('info').innerHTML = output;
-    document.getElementById('name').innerHTML = name;
-    document.getElementById('cv-photo').innerHTML = image;
+
+  content.results.forEach((lists)=>{
+    
+    console.log(lists)
+    name = `${lists.name.first} ${lists.name.last}`
+    image =`<img class="photo-large" src="${lists.picture.large}">`
+    phone = `${lists.cell}`
+    email = `${lists.email}`
+    city = `${lists.location.city}`
+
   });
- };
- 
- play =(url)=> {
-  new Audio(url).play();
+  
+  
+  document.getElementById('phone').innerHTML= phone;
+  document.getElementById('cv-photo').innerHTML = image;
+  document.getElementById('name').innerHTML = name;
+  document.getElementById('email').innerHTML = email;
+  document.getElementById('city').innerHTML = city;
 }
- 
-fetchData();
-displayInfo();
-displayInfo2();
-play();
+var data = await fetchData();
+renderContent(data);
